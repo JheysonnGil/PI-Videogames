1f6e154735b9e4f4000502f3c54e2a919bc1d6aa
Jheysonn Gil <80410419+rennemetter@users.noreply.github.com>
2022-06-23 12:41:03 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+// orrar test
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
